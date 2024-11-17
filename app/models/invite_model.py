1267f3ace954a0a4e8a57091ee0a6fbf6ddc3a78
Dylan Da Costa <dylan2222@gmail.com>
2024-11-16 21:19:32 -0500

    updated invite model to remove relatiosnhip to users model

--- app/models/invite_model.py
+++ app/models/invite_model.py
@@ -1,9 +1,8 @@
 import uuid
 from sqlalchemy import (ForeignKey, Column, Boolean, Integer, String)
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy.orm import Mapped
 from sqlalchemy.dialects.postgresql import UUID
 from app.database import Base
-from app.models.user_model import User
 
 class Invitation(Base):
     __tablename__ = 'invitations'
@@ -12,5 +11,3 @@ class Invitation(Base):
     invite_code: Mapped[str] = Column(String, unique=True, index=True)
     user_id: Mapped[uuid.UUID] = Column(UUID(as_uuid=True), ForeignKey("users.id"))
     used: Mapped[bool] = Column(Boolean, default=False)
-
-    user: Mapped[User] = relationship("User", back_populates="invitations")
