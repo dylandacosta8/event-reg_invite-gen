e604edc63b8566630db482ea9c9a7aae0c62fc84
Dylan Da Costa <dylan2222@gmail.com>
2024-11-18 11:36:57 -0500

    includes newly created invite routes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from starlette.responses import JSONResponse
 from starlette.middleware.cors import CORSMiddleware  # Import the CORSMiddleware
 from app.database import Database
 from app.dependencies import get_settings
-from app.routers import user_routes
+from app.routers import user_routes, invite_routes
 from app.utils.api_description import getDescription
 from app.minio_setup import create_minio_bucket
 
@@ -41,5 +41,4 @@ async def exception_handler(request, exc):
     return JSONResponse(status_code=500, content={"message": "An unexpected error occurred."})
 
 app.include_router(user_routes.router)
-
-
+app.include_router(invite_routes.router)
